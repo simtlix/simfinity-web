42505fb211d6158397e56f3d754a2ee6a10bc16d
Monserrat Diaz <mdiaz@technisys.com>
2021-03-21 22:56:28 -0300

    Add a sidebar

--- src/direflow-components/App.js
+++ src/direflow-components/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout, Menu } from "antd";
 import { Styled } from "direflow-component";
 import styles from "./App.css";
 
 const App = () => {
   const { Header, Content, Footer, Sider } = Layout;
+  const [collapsed, setCollapsed] = useState(false);
+
+  const onCollapse = (collapsed) => {
+    setCollapsed(collapsed);
+  };
   return (
     <Styled styles={styles}>
       <div>
         <Layout style={{ minHeight: "100vh" }}>
-          <Sider collapsible collapsed={true}>
+          <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
             <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-              <Menu.Item key="1">Option 1</Menu.Item>
-              <Menu.Item key="2">Option 2</Menu.Item>
+              <Menu.Item key="1">Entidades</Menu.Item>
+              <Menu.Item key="2">Option 1</Menu.Item>
+              <Menu.Item key="3">Option 2</Menu.Item>
               <Menu.Item key="9">Files</Menu.Item>
             </Menu>
           </Sider>
