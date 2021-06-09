2dad64caa46b843a8f0b8712b8f8cbb7b2876b33
Emiliano Scavuzzo <emiacc@hotmail.com>
2021-06-09 12:22:33 -0300

    Add stateMachine info the the metadata

--- src/direflow-components/utils.js
+++ src/direflow-components/utils.js
@@ -19,6 +19,7 @@ export const requestEntities = async (url) => {
                     name
                     description
                     extensions {
+                      stateMachine
                       relation {
                         embedded
                         connectionField
