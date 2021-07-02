e491a57dd59fafe3b1e0f4be8397a8d2f2dbd577
Claudio Gonzalez <gonzalez74@avaya.com>
2021-07-02 17:48:27 -0300

    initial commit for internal collections

--- src/direflow-components/Form/Form.js
+++ src/direflow-components/Form/Form.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { Form as FormAntd, Button, Row, Col} from "antd";
+import { Form as FormAntd, Button, Row, Col, Collapse} from "antd";
 import { FormItems } from "./FormItems";
+import Collection from "./Collection";
 import 'antd/dist/antd.css';
+import { useIntl } from "react-intl";
+
+const { Panel } = Collapse; 
 
 const layout = {
   labelCol: {
@@ -17,7 +21,8 @@ const layout = {
 const Form = ({ displayEntity = null, visible=true, name, openForResultHandler, initialValues }) => {
 
   const [form] = FormAntd.useForm();
- 
+  const intl = useIntl()
+
   if(!displayEntity){
     return;
   }
@@ -26,22 +31,51 @@ const Form = ({ displayEntity = null, visible=true, name, openForResultHandler, 
     (field) => field.name !== "id" && field.type.kind !== "LIST"
   )
 
+  const collectionFields = displayEntity.fields.filter(
+    (field) => field.name !== "id" && field.type.kind === "LIST" && field.type.ofType.kind === 'OBJECT'
+  )
+
 
   return (
-    <Row style={{display: visible?"":"none"}}>
-      <Col span={24}>
-        <FormAntd {...layout} form={form} initialValues={initialValues} name={name} >
-         <FormItems fields={filteredFields} form={form} openForResult={openForResultHandler} entity={displayEntity}></FormItems>
-          {displayEntity != null ? (
-            <FormAntd.Item>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </FormAntd.Item>
-          ) : null}
-          </FormAntd>
-      </Col>
-    </Row>
+    <>
+      <Row style={{display: visible?"":"none"}}>
+        <Col span={24}>
+          <FormAntd {...layout} form={form} initialValues={initialValues} name={name} >
+          <FormItems fields={filteredFields} form={form} openForResult={openForResultHandler} entity={displayEntity}></FormItems>
+          { collectionFields.length > 0 && 
+              <Row>
+                <Col span={24}>
+                  <Collapse>
+                  
+                  {
+                    collectionFields.map((field, index) => {
+                      let data = []
+                      if(initialValues){
+                        data = initialValues[field.name]
+                      }
+                      return (<Panel header={intl.formatMessage({id:`entity.${displayEntity.name}.fields.${field.name}`, defaultMessage:field.name})} key={index}>
+                                  <Collection key={field.name} field={field} data={data} />
+                              </Panel>)
+                    })
+                  }
+                  </Collapse>
+                </Col>
+              </Row>
+          }
+
+
+            {displayEntity != null ? (
+              <FormAntd.Item>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </FormAntd.Item>
+            ) : null}
+            </FormAntd>
+        </Col>
+      </Row>
+      
+    </>
   );
 };
 
