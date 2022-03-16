f0cecf6d3e19e16ab918c293249a5b84cdb999c0
Fede Roca <froca@simtlix.com>
2022-03-16 02:02:00 -0300

    feat(form): change inputs size and field layout
    change inputs size and field layout

--- src/direflow-components/Form/FormItems.js
+++ src/direflow-components/Form/FormItems.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Field } from './Field';
-
+import { Col } from 'antd';
 export const FormItems = ({
   fields,
   parentFieldName,
   form,
   openForResult,
   entity,
-  span = 12,
+  span = 24,
 }) => {
   const renderFormFields = fields.map((field) => {
     const fieldName = parentFieldName
       ? [parentFieldName, field.name]
       : field.name;
     return (
-      <Field
-        parentFieldName={parentFieldName}
-        entity={entity}
-        form={form}
-        openForResult={openForResult}
-        field={field}
-        key={fieldName}
-        span={span}
-      ></Field>
+      <Col span={span} key={fieldName}>
+        <Field
+          parentFieldName={parentFieldName}
+          entity={entity}
+          form={form}
+          openForResult={openForResult}
+          field={field}
+        ></Field>
+      </Col>
     );
   });
 
